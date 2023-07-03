b6abddec0de6d900942f0f5f2fd3d6b89191452e
ehabsalhi <ehab1salhe@gmail.com>
2023-07-03 00:12:06 +0000

    changing the test file

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -1,34 +1,35 @@
-const supertest = require('supertest')
-require('dotenv').config()
-const base64 = require("base-64")
-const { userSequelize } = require('../src/model')
-const { app } = require('../src/server')
-const muke = supertest(app)
+// const supertest = require('supertest')
+// require('dotenv').config()
+// const base64 = require("base-64")
+// const { userSequelize } = require('../src/model')
+// const { app } = require('../src/server')
+// const muke = supertest(app)
 
 
-  beforeAll(async () => {
-     await userSequelize.sync()
-})
+//   beforeAll(async () => {
+//      await userSequelize.sync()
+// })
 
-afterAll(async () =>{
-     await userSequelize.drop()
-})
+// afterAll(async () =>{
+//      await userSequelize.drop()
+// })
 
-describe('server test' , () => {
+// describe('server test' , () => {
 
 
-     it('signup test' , async () =>{
-          const name = 'ehab1'
-          const res = await muke.post('/signup').send({
-               username: name,
-               password : '123123',
-               role: 'admin'
-          })
+//      it('signup test' , async () =>{
+//           const name = 'ehab1'
+//           const res = await muke.post('/signup').send({
+//                username: name,
+//                password : '123123',
+//                role: 'admin'
+//           })
+//           console.log(JSON.parse(res.text));
 
-          expect(res.statusCode).toBe(201)
-          expect((JSON.parse(res.text).user.username)).toBe(name)
+//           expect(res.statusCode).toBe(201)
+//           expect((JSON.parse(res.text).user.username)).toBe(name)
 
-     })
+//      })
 
 //     const base = base64.encode('ehab1:123123') 
 //      it('signin test' , async () =>{
@@ -133,4 +134,136 @@ describe('server test' , () => {
 //      })
 
 
-})
+// })
+
+
+'use strict';
+
+process.env.SECRET = "TEST_SECRET";
+
+// const supertest = require('supertest')
+require('dotenv').config()
+const base64 = require("base-64")
+// const muke = supertest(app)
+
+
+// const { userSequelize } = require('../../../../src/auth/models');
+// const server = require('../../../../src/server.js').server;
+
+const { app } = require('../src/server')
+const { userSequelize } = require('../src/model')
+const supertest = require('supertest');
+const mockRequest = supertest(app);
+
+
+let userData = {
+  testUser: { username: 'user', password: 'password' , role : 'admin'},
+};
+let accessToken = null;
+
+beforeAll(async () => {
+  await userSequelize.sync();
+});
+afterAll(async () => {
+  await userSequelize.drop();
+});
+
+describe('Auth Router', () => {
+
+  it('Can create a new user', async () => {
+
+    const response = await mockRequest.post('/signup').send(userData.testUser);
+    const userObject = response.body;
+
+    expect(response.status).toBe(201);
+//     expect(userObject.token).toBeDefined();
+    expect(userObject.user.id).toBeDefined();
+    expect(userObject.user.username).toEqual(userData.testUser.username);
+  });
+
+//   it('Can signin with basic auth string', async () => {
+//     let { username, password } = userData.testUser;
+
+//     const response = await mockRequest.post('/signin')
+//       .auth(username, password);
+
+//     const userObject = response.body;
+//     expect(response.status).toBe(200);
+//     expect(userObject.token).toBeDefined();
+//     expect(userObject.user.id).toBeDefined();
+//     expect(userObject.user.username).toEqual(username);
+//   });
+
+//   it('Can signin with bearer auth token', async () => {
+//     let { username, password } = userData.testUser;
+
+//     // First, use basic to login to get a token
+//     const response = await mockRequest.post('/signin')
+//       .auth(username, password);
+
+//     accessToken = response.body.token;
+
+//     // First, use basic to login to get a token
+//     const bearerResponse = await mockRequest
+//       .get('/users')
+//       .set('Authorization', `Bearer ${accessToken}`);
+
+//     // Not checking the value of the response, only that we "got in"
+//     expect(bearerResponse.status).toBe(200);
+//   });
+
+//   it('basic fails with known user and wrong password ', async () => {
+
+//     const response = await mockRequest.post('/signin')
+//       .auth('admin', 'xyz')
+//     const { user, token } = response.body;
+
+//     expect(response.status).toBe(403);
+//     expect(response.text).toEqual("Invalid Login");
+//     expect(user).not.toBeDefined();
+//     expect(token).not.toBeDefined();
+//   });
+
+//   it('basic fails with unknown user', async () => {
+
+//     const response = await mockRequest.post('/signin')
+//       .auth('nobody', 'xyz')
+//     const { user, token } = response.body;
+
+//     expect(response.status).toBe(403);
+//     expect(response.text).toEqual("Invalid Login");
+//     expect(user).not.toBeDefined();
+//     expect(token).not.toBeDefined();
+//   });
+
+//   it('bearer fails with an invalid token', async () => {
+
+//     // First, use basic to login to get a token
+//     const response = await mockRequest.get('/users')
+//       .set('Authorization', `Bearer foobar`)
+//     const userList = response.body;
+
+//     // Not checking the value of the response, only that we "got in"
+//     expect(response.status).toBe(403);
+//     expect(response.text).toEqual("Invalid Login");
+//     expect(userList.length).toBeFalsy();
+//   });
+
+//   it('Succeeds with a valid token', async () => {
+
+//     const response = await mockRequest.get('/users')
+//       .set('Authorization', `Bearer ${accessToken}`);
+
+//     expect(response.status).toBe(200);
+//     expect(response.body).toBeTruthy();
+//     expect(response.body).toEqual(expect.anything());
+//   });
+
+//   it('Secret Route fails with invalid token', async () => {
+//     const response = await mockRequest.get('/secret')
+//       .set('Authorization', `bearer accessgranted`);
+
+//     expect(response.status).toBe(403);
+//     expect(response.text).toEqual("Invalid Login");
+//   });
+});
